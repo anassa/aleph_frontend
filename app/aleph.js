c495a080d39e7a16f8e2be39c892d2d60ec7730c
Neri Guidi <neri3004@gmail.comi>
2014-08-17 12:58:54 -0300

    Documentacion de Aleph, Ventas y Frame

--- app/aleph.js
+++ app/aleph.js
@@ -14,6 +14,7 @@ define(
 	]
 ,	function()
 	{
+		//	Aleph.Home va a controlar la gestion de la APP. Desde el Login hasta el Logout.
 		can.Control(
 			'Aleph.Home'
 		,	{
@@ -25,66 +26,48 @@ define(
 		,	{
 				init: function(element,options)
 				{
+					//	Genera un DIV para contener el topbar
 					this.$topbar
 					=	can.$('<div>')
 							.appendTo(
 								element
 							)
-
-					this.$content
-					=	can.$('<div>')
-							.appendTo(
-								element
-							)
-
+					//	Agrega el Footbar
 					can.append(
 						element
 					,	can.view(
 							'views/footbar/footbar.mustache'
 						)
 					)
-
+					//	Borra el Hash
 					location.hash = ""
-
+					//	Setea la forma que va a tener nuestro Hash, #!:page donde :page es variable y tomara la forma que se le envie
 					can.route(':'+options.route, {});
-					
+					//	Avisa que el hash esta listo para escuchar cambios
 					can.route.ready();
-					
-					new	Frame.Menu(
-						this.$topbar
-					,	{
-							view:	'views/topbar/topbar.mustache'
-						,	options:
-							{
-								brand:			'Aleph'
-							,	options:		[]
+					//	Insterta el Topbar dentro del DIV generado antes
+					this.topbar
+					=	new	Frame.Menu(
+							this.$topbar
+						,	{
+								view:			'views/topbar/topbar.mustache'
+							,	options:
+								{
+									brand:		'Aleph'
+								,	options:	[]
+								}
+							,	default_option:	'login'
+							,	event_prefix:	'aleph.render'
+							,	route:			options.route
 							}
-						,	default_option:	'login'
-						,	event_prefix:	'aleph.render'
-						,	route:			options.route
-						}
-					)
-
-					//this._render_login()
+						)
 				}
-
-			,	'aleph.menu.update': function(el,ev,data)
+				//	Evento que al ser escuchado renderiza el Login
+			,	'aleph.render.login': function(el,ev,data)
 				{
-					this
-						.$topbar
-							.trigger(
-								'frame.menu.update'
-							,	data
-							)
-				}
-
-			,	'aleph.render.login': function()
-				{
+					//	Ev.target es el contenido donde se lanzo el evento, en ese elemento se va a instanciar el Login
 					new Frame.Login(
-						can.$('<div>')
-							.appendTo(
-								this.$content.empty()
-							)
+						can.$(ev.target)
 					,	{
 							view:		'views/login/login.mustache'
 						,	view_alert:	'views/login/alert.mustache'
@@ -97,34 +80,53 @@ define(
 						}
 					)
 				}
-
-			,	_render_content: function(content,user)
+				//	Evento que al ser escuchado renderiza el contenido dependiendo del usuario
+			,	'aleph.render.content': function(el,ev,user)
 				{
-					//	Elimino el contenido del DIV $content
-					this.$content.empty()
-					//	Instancio el controlador que se encuentra en la variable content (Ventas, Compras, etc)
-					new	Aleph[content](
-						this.$content
+					//	User.getProfile devuelve el nombre del perfil del usuario, por ejemplo Ventas.
+					//	Al hacer Aleph["Ventas"] se hace referencia al controlador Aleph.Ventas.
+					//	Recordar que es Aleph es un objeto y "Ventas" sera la key a acceder
+					new Aleph[user.getProfile()](
+						can.$(ev.target)
 					,	{
 							user:	user
 						}
 					)
 				}
-
+				//	Evento que al ser escuchado renderiza profile
+			,	'aleph.render.profile': function(el,ev)
+				{
+					//	Ev.target es el contenido donde se lanzo el evento, en ese elemento se va a instanciar profile
+					console.log("Profile",arguments)
+				}
+				//	Evento que al ser escuchado renderiza logout
+			,	'aleph.render.logout': function(el,ev)
+				{
+					//	Ev.target es el contenido donde se lanzo el evento, en ese elemento se va a instanciar logout
+					console.log("Logout",arguments)
+				}
+				//	Funcion a llamar cuando se solicita el login
 			,	signIn: function(formData)
 				{
 					return	Aleph.Model.User.signIn(formData)
 				}
-
+				//	Funcion a llamar cuando se solicita el logout
 			,	signOut: function()
 				{
 					return	true
 				}
-
-			,	'frame.login.signin.success': function(el,el,user)
+				//	Evento a escuchar i el login es satisfactorio
+			,	'frame.login.signin.success': function(el,ev,user)
 				{
-					if	(!_.isEmpty(user.getProfile()))
-						this._render_content(can.capitalize(user.getProfile()),user)
+					//	Ev.target contiene el DIV donde se va a instanciar el contenido.
+					//	Vacio el elemento
+					can.$(ev.target).empty()
+					//	Triggeo el evento que renderizara el contenido dependiendo el usuario
+					can.$(ev.target)
+						.trigger(
+							'aleph.render.content'
+						,	user
+						)
 				}
 			}
 		)
--- app/venta/venta.js
+++ app/venta/venta.js
@@ -6,6 +6,7 @@ define(
 	]
 ,	function()
 	{
+		//	Aleph.Ventas sera el Control principal de todo el modulo de Ventas
 		can.Control(
 			'Aleph.Ventas'
 		,	{
@@ -15,12 +16,14 @@ define(
 				}
 			}
 		,	{
+				//	Cuando se inicia por primera vez debera actualizar las opciones del Topbar
 				init: function(element,options)
 				{
+					//	Envia al menu las nuevas opciones para su controlador
 					this
 						.element
 							.trigger(
-								'aleph.menu.update'
+								'frame.menu.update'
 							,	{
 									viewData:
 									{
@@ -59,6 +62,43 @@ define(
 									}
 								}
 							)
+				}
+
+				//	Evento que al ser escuchado renderiza Ventas
+			,	'aleph.render.ventas': function(el,ev)
+				{
+					//	Ev.target es el contenido donde se lanzo el evento, en ese elemento se va a instanciar ventas
+					console.log("Ventas",arguments)
+				}
+			//	Evento que al ser escuchado renderiza Cambios
+			,	'aleph.render.cambios': function(el,ev)
+				{
+					//	Ev.target es el contenido donde se lanzo el evento, en ese elemento se va a instanciar cambios
+					console.log("Cambios",arguments)
+				}
+			//	Evento que al ser escuchado renderiza Clientes
+			,	'aleph.render.clientes': function(el,ev)
+				{
+					//	Ev.target es el contenido donde se lanzo el evento, en ese elemento se va a instanciar clientes
+					console.log("Clientes",arguments)
+				}
+			//	Evento que al ser escuchado renderiza Descuentos
+			,	'aleph.render.descuentos': function(el,ev)
+				{
+					//	Ev.target es el contenido donde se lanzo el evento, en ese elemento se va a instanciar descuentos
+					console.log("Descuentos",arguments)
+				}
+			//	Evento que al ser escuchado renderiza Articulos
+			,	'aleph.render.articulos': function(el,ev)
+				{
+					//	Ev.target es el contenido donde se lanzo el evento, en ese elemento se va a instanciar articulos
+					console.log("Articulos",arguments)
+				}
+			//	Evento que al ser escuchado renderiza Formas de Pago
+			,	'aleph.render.formas_de_pago': function(el,ev)
+				{
+					//	Ev.target es el contenido donde se lanzo el evento, en ese elemento se va a instanciar formas_de_pago
+					console.log("Formas de Pago",arguments)
 				}
 			}
 		)
