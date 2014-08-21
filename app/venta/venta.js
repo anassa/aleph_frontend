faa092ba3818374e1e8a35a4a18beed38693c843
Neri Guidi <neri3004@gmail.comi>
2014-08-21 11:33:47 -0300

    Estilos de Ventas

--- app/venta/venta.js
+++ app/venta/venta.js
@@ -1,6 +1,11 @@
 define(
 	[
+	//	Librerias de Can
 		'lib/util'
+	//	Modelo de Articulos
+	,	'models/item'
+	//	Estilos de Venta
+	,	'css!styles/sidebar'
 	]
 ,	function()
 	{
@@ -11,13 +16,70 @@ define(
 				defaults:
 				{
 					user:	undefined
+				,	view:	'views/ventas/ventas.mustache'
 				}
 			}
 		,	{
 				//	Cuando se inicia por primera vez debera actualizar las opciones del Topbar
 				init: function(element,options)
 				{
-					console.log("Ventas.Venta")
+					can.append(
+						element
+					,	can.view(
+							options.view
+						)
+					)
+					this.$form
+					=	this.element.find('#searchItems')
+
+					this.$content
+					=	this.element.find('.main-content')
+				}
+
+			,	generateQuery: function()
+				{
+					var	formData
+					=	can.getFormData(
+							this.$form
+						)
+
+					return	{
+								name:
+								{
+									contains:	formData.name
+								}
+							,	code:
+								{
+									contains:	formData.code
+								}
+							}
+				}
+
+			,	updateTable: function()
+				{
+					console.log(arguments)
+				}
+
+			,	notifyError: function()
+				{
+					console.log(arguments)
+				}
+
+			,	'button.search-items click': function(el,ev)
+				{
+					Aleph
+						.Model
+							.Item
+								.findAll(
+									{
+										where:	this.generateQuery()
+									,	limit:	10
+									,	sort:	'name DESC'
+									}
+								).then(
+									can.proxy(this.updateTable,this)
+								,	can.proxy(this.notifyError,this)
+								)
 				}
 			}
 		)
