25813b189ceade123ece8474909d026665be4be7
DMColussi <diegomartin010@gmail.com>
2016-12-13 23:57:52 -0300

    fix #36 Menu Principal. Funcionalidad Iconos.
    Funcionan los iconos de la pantalla principal

--- src/home/home.js
+++ src/home/home.js
@@ -27,7 +27,17 @@ export const ViewModel = Map.extend(
 				value:	undefined
 			,	type:	'string'
 			}
+			// Valor que van a tomar los diferentes submodulos
+			,moduloSelect:{
+				value: undefined
+			}
 		}
+	// Lo que sucede cuando cargo la pagina
+	, init: function(){
+		// Oculto la movida de submodulos.
+		$("#subModulo").hide()
+		// $("#returnButton").remove()
+	}
 	,	updateUser: function(el)
 		{
 			var	$button
@@ -111,6 +121,179 @@ export const ViewModel = Map.extend(
 						);
 					}
 				)
+		}
+		, volverPrincipal: function(elem){
+			// alert(subMapping[modSelected])
+			// $('#iconos').hide()
+			$('#iconos').show('slow')
+			// $('#iconos').animate({
+			//     // opacity: 0.25,
+			//     // left: "+=50",
+			//     width: "100%"
+			//  }, 500, function() {
+			//     // Animation complete.
+			//  });
+			// // $('#iconos').append('<p id="movida">holamundo</p>')
+			$('#subModulo').hide('slow')
+
+		}
+		// Comportamiento cuando tocamos botones
+		, clickIcon: function(modSelected){
+			// Creamos un array de JSONS. Practica pco saludable.
+			var subMapping = {
+				ventas:[
+					{
+						nombre:"Nueva Venta" 
+						,icon:' fa-dollar '
+						,section:'nueva_venta'
+						,modulo:'ventas'
+					},
+					{
+						nombre:"Registros Venta" 
+						,icon:' fa-list-alt '
+						,section:'registros_venta'
+						,modulo:'ventas'
+					},
+					{	nombre:"Nueva Solicitud"
+						,icon:'fa-shopping-cart'
+						,section:'nueva_solicitud'
+						,modulo:'ventas'
+					},
+					{
+						nombre:"Registros Solicitud" 
+						,icon:'fa-list-alt'
+						,section:'registros_solicitud'
+						,modulo:'ventas'
+					},
+					{	
+						nombre:"Nuevo Cliente" 
+						,icon:'fa-male '
+						,section:'nuevo_cliente'
+						,modulo:'ventas'
+					},
+					{
+						nombre:"Registros Cliente"
+						,icon:'fa-list-alt ' 
+						,section:'registros_cliente'
+						,modulo:'ventas'
+					},
+					// {
+					// 	nombre:"Nueva Forma de Pago"
+					// 	,icon:"fa-credit-card" 
+					// 	,section:'nueva_forma_de_pago'
+					// 	,modulo:'ventas'
+					// },
+					// {
+					// 	nombre:"Registros Forma de Pago" 
+					// 	,icon:"fa-list-alt"
+					// 	,section:'registros_forma_de_pago'
+					// 	,modulo:'ventas'
+					// },
+					{
+						nombre:"Reportes"
+						,icon:"fa-file-text"
+						,section:'reportes'
+						,modulo:'ventas'
+					},
+				]
+				// Fin modulo de ventas
+				// Modulo de compras
+
+				,compras:[
+					{
+						nombre:"Nueva Orden de Compra"
+						,icon:' fa-truck   ' 
+						,section:'nueva_oc'
+						,modulo:'compras'
+					},
+					{
+						nombre:"Registros Orden de Compra"
+						,icon:' fa-list-alt ' 
+						,section:'registros_oc'
+						,modulo:'compras'
+					},
+					{
+						nombre:"Nuevo Proveedor"
+						,icon:' fa-briefcase ' 
+						,section:'nuevo_proveedor'
+						,modulo:'compras'
+					},
+					{
+						nombre:"Registros Proveedor"
+						,icon:' fa-list-alt ' 
+						,section:'registros_proveedor'
+						,modulo:'compras'
+					},
+					{
+						nombre:"Reportes"
+						,icon:' fa-file-text ' 
+						,section:'reportes'
+						,modulo:'compras'
+					}
+				]
+				,stock:[
+					{
+						nombre:"Nuevo Articulo"
+						,icon:' fa-plus ' 
+						,section:'nuevo_articulo'
+						,modulo:'stock'
+					},
+					{
+						nombre:"Registros Articulo"
+						,icon:' fa-list-alt '
+						 ,section:'registros_articulo'
+						 ,modulo:'stock'
+					},
+					// {
+					// 	nombre:"Nuevo Remito"
+					// 	,icon:' fa-clipboard ' 
+					// 	,section:'nuevo_remito'
+					// 	,modulo:'stock'
+					// },
+					// {
+					// 	nombre:"Registros Remito"
+					// 	,icon:' fa-list-alt ' 
+					// 	,section:'registros_remito'
+					// 	,modulo:'stock'
+					// },
+					// {
+					// 	nombre:"Reportes"
+					// 	,icon:'fa-file-text '
+					// 	, section:'reportes'
+					// 	,modulo:'stock'
+					// }
+				]
+				,administracion:[
+					{
+						nombre:"Nuevo Usuario"
+						,icon:' fa-user   ' 
+						,section:'nuevo_usuario'
+						,modulo:'administracion'
+					},
+					{
+						nombre:"Registros Usuario"
+						,icon:' fa-group   '
+						,section:'registros_usuario'
+						,modulo:'administracion'
+					}
+				]				
+			}
+
+			this.attr('moduloSelect',subMapping[modSelected]);
+			this.attr('moduloSelect.volver',modSelected);
+			// alert(subMapping[modSelected])
+
+			// $('#iconos').animate({
+			//     // opacity: 0.25,
+			//     // left: "+=50",
+			//     width: "45%"
+			//  }, 500, function() {
+			//     // Animation complete.
+			//  });
+			$('#iconos').hide('slow')
+			$("#subModulo").hide()
+			$('#subModulo').hide().show('slow')
+
 		}
 	}
 );
